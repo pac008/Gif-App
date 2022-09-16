de3443512f5a5a1fd1bd5fdd16b025127fc23937
Miguel <miguel-angel.herrera-alcantar@altran.com>
2022-09-16 17:28:53 +0200

    api-fixed

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=BRDdknVVlH9Ls5Uvdsa0Mx6vEBUnPO1l&q=${category}&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Api_aqui&q=${category}&limit=20`
 
     const resp = await fetch(url)
     const {data} = await resp.json()
